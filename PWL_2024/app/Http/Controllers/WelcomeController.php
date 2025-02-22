8002561b7372946b836bedc72254bfc224246213
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-02-22 19:47:23 +0700

    Jobsheet 2_Meneruskan data ke view

--- PWL_2024/app/Http/Controllers/WelcomeController.php
+++ PWL_2024/app/Http/Controllers/WelcomeController.php
@@ -12,9 +12,15 @@ class WelcomeController extends Controller
     }
 
     //menambahkan method greeting() yang akan mengembalikan view dengan nama hello.blade.php dan mengirim data name ke view tersebut
+    // public function greeting(){
+    //     return view('blog.hello', ['name' => 'Muhammad Rifda Musyaffa\'']);
+    // }
+
+    //mengubah method greeting() yang akan mengembalikan view dengan nama hello.blade.php dan mengirim data name dan occupation ke view tersebut
+    //menggunakan method with()
     public function greeting(){
-        return view('blog.hello', ['name' => 'Muhammad Rifda Musyaffa\'']);
+        return view('blog.hello')
+        ->with('name', 'Muhammad Rifda Musyaffa')
+        ->with('occupation', 'Mahasiswa');
     }
-
-
 }
--- PWL_2024/resources/views/blog/hello.blade.php
+++ PWL_2024/resources/views/blog/hello.blade.php
@@ -1,6 +1,8 @@
 <!-- View pada resources/views/hello.blade.php -->
 <html>
 <body>
+    {{-- {{ }} digunakan sebagai blade echo statement untuk mencetak data ke dalam tampilan HTML secara aman. --}}
     <h1>Hello, {{ $name }}</h1>
+    <h1>You are {{ $occupation }}</h1>
 </body>
 </html>
