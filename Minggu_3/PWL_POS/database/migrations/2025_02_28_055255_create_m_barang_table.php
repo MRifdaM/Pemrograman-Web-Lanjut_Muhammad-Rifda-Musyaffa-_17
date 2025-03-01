d84f04d3e7c6aa99f41c5705b99775ddc7663c17
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-03-01 17:56:00 +0700

    Merapihkan struktur direktori

---
+++ Minggu_3/PWL_POS/database/migrations/2025_02_28_055255_create_m_barang_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id');
+            $table->unsignedBigInteger('kategori_id')->index();
+            $table->string('barang_kode');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+    }
+};
