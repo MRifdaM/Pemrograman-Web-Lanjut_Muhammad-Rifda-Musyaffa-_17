14b8f3019c2eac6d41710d19f30a0cf298eba2c6
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-03-02 23:22:45 +0700

    Jobsheet 3_Praktikum Ke-6 _Implementasi Eloquent ORM_Menambahkan data menggunakan Eloquent ORM serta Memperbaiki kode untuk menambahkan data baru Menggunakan Eloquent ORM

--- Minggu_3/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_3/PWL_POS/app/Http/Controllers/UserController.php
@@ -12,14 +12,21 @@ class UserController extends Controller
     public function index(){
 
         //Menambahkan data baru menggunakan Eloquent
-        $data = [
-            'username' => 'Customer',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'), // class untuk mengenkripsi/hash password
-            'level_id' => 5
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'), // class untuk mengenkripsi/hash password
+        //     'level_id' => 5
+        // ];
+
+        // UserModel::insert($data); //Memasukkan atau menambahkan data baru mengguankan Eloquent ORM
+
+        //Merubah data yang sudah ada menggunakan Eloquent
+        $data =[
+            'nama' => 'Pelanggan Pertama'
         ];
 
-        UserModel::insert($data); //Memasukkan atau menambahkan data baru mengguankan Eloquent ORM
+        UserModel::where('username', 'customer-1')->update($data); //Merubah data yang sudah ada menggunakan Eloquent ORM
 
         //Mengambil semua data menggunakan Eloquent
         $user = UserModel::all();
