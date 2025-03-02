c6b52a486c2fd5922d5e42225c52e9da56213ebe
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-03-02 23:13:05 +0700

    Jobsheet 3_Praktikum 6_Implementasi Eloquent ORM_Menambahkan kode untuk memasukkan data menggunakan Eloquent ORM

--- Minggu_3/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_3/PWL_POS/app/Http/Controllers/UserController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers;
 //memanggil UserModel
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index(){
+
         //Menambahkan data baru menggunakan Eloquent
+        $data = [
+            'username' => 'Customer',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'), // class untuk mengenkripsi/hash password
+            'level_id' => 5
+        ];
+
+        UserModel::insert($data); //Memasukkan atau menambahkan data baru mengguankan Eloquent ORM
+
+        //Mengambil semua data menggunakan Eloquent
         $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
