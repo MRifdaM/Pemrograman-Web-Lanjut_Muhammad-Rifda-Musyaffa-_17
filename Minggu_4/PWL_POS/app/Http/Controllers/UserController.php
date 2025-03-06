8130c69f067f481947556c82cab64eaeb8c9cdff
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-03-06 08:02:37 +0700

    Jobsheet 4_Praktikum 2.1_Retrieving Single Models_Menggunakan method findOr() dengan nilai yang tdiak ada dalam database tabel m_user

--- Minggu_4/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_4/PWL_POS/app/Http/Controllers/UserController.php
@@ -60,11 +60,18 @@ class UserController extends Controller
             //-------------------------------------------------Praktikum 2.1------------------------------------------------------
 
             // $user = UserModel::find(1); //Mencari data pengguna dengan primary key ID = 1 di dalam database menggunakan Eloquent ORM
+
             // $user = UserModel::where('level_id', 1)->first(); //Mengambil satu data pertama dari tabel yang memiliki level_id = 1 menggunakan Eloquent
+
             // $user = UserModel::firstWhere('level_id', 1); //shortcut dari where()->first()
-            $user = UserModel::findOr(1, ['username', 'nama'], function(){ //Mencari data pengguna dengan primary key tetapi dengan fallback (alternatif) jika data tidak ditemukan
-                //Jika data ditemukan, hanya kolom username dan nama yang akan diambil.
-                abort(404); //Jika data tidak ditemukan, maka akan menampilkan error 404
+
+            // $user = UserModel::findOr(1, ['username', 'nama'], function(){ //Mencari data pengguna dengan primary key tetapi dengan fallback (alternatif) jika data tidak ditemukan
+            //     //Jika data ditemukan, hanya kolom username dan nama yang akan diambil.
+            //     abort(404); //Jika data tidak ditemukan, maka akan menampilkan error 404
+            // });
+
+            $user = UserModel::findOr(20, ['username', 'nama'], function () {
+                abort(404);
             });
             return view('user', ['data' => $user]);
     }
