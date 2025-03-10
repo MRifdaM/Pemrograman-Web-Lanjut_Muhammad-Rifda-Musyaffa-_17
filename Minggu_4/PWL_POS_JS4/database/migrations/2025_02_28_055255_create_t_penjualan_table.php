cac6d002e53cc49009a784c0929a74d1b35cc068
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-03-10 22:25:23 +0700

    Perbaikan file migration

--- Minggu_4/PWL_POS_JS4/database/migrations/2025_02_28_055255_create_t_penjualan_table.php
+++ Minggu_4/PWL_POS_JS4/database/migrations/2025_02_28_055255_create_t_penjualan_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->datetime('tanggal_penjualan');
             $table->timestamps();
 
-            $table->foreign('user_id')->references('user_id')->on('m_user')->onDelete('cascade');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
 
