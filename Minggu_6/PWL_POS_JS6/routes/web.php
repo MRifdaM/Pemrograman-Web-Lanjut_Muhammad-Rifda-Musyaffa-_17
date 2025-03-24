5e62c1429c929fa223f13b5ead33b7acc1fd8edd
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-03-24 12:25:07 +0700

    Penambahan Jobsheet 6 Praktikum 2: pop-up modal Ajax untuk edit data user

--- Minggu_6/PWL_POS_JS6/routes/web.php
+++ Minggu_6/PWL_POS_JS6/routes/web.php
@@ -44,11 +44,17 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('/list', [UserController::class, 'list']);        // menampilkan data user dalam bentuk json untuk datatables
     Route::get('/create', [UserController::class, 'create']);    // menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']);          // menyimpan data user baru
+    //---------------------------------------------Jobhseet 5 Praktikum 1------------------------------------------
     Route::get('/create_ajax', [UserController::class, 'create_ajax']);  // menampilkan halaman form tambah user Ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']);  // menyimpan data user baru Ajax
+    //-------------------------------------------------------------------------------------------------------------
     Route::get('/{id}', [UserController::class, 'show']);        // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
     Route::put("/{id}", [UserController::class, 'update']);       // menyimpan perubahan data user
+    //---------------------------------------------Jobhseet 5 Praktikum 2------------------------------------------
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user Ajax
+    //-------------------------------------------------------------------------------------------------------------
     Route::delete('/{id}', [UserController::class, 'destroy']);  // menghapus data user
 });
 
