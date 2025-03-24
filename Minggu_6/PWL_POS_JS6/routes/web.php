d60cab0307aa9608d9b1862eade7a70d69eb5b54
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-03-25 00:53:06 +0700

    Penambahan Jobsheet 6 Tugas soal 2: Implementasi Ajax Form dan Client Validation dengan jQuery Validation pada tabel m_kategori

--- Minggu_6/PWL_POS_JS6/routes/web.php
+++ Minggu_6/PWL_POS_JS6/routes/web.php
@@ -44,17 +44,17 @@ Route::group(['prefix' => 'user'], function () {
     Route::post('/list', [UserController::class, 'list']);        // menampilkan data user dalam bentuk json untuk datatables
     Route::get('/create', [UserController::class, 'create']);    // menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']);          // menyimpan data user baru
-    //---------------------------------------------Jobhseet 5 Praktikum 1------------------------------------------
+    //---------------------------------------------Jobhseet 6 Praktikum 1------------------------------------------
     Route::get('/create_ajax', [UserController::class, 'create_ajax']);  // menampilkan halaman form tambah user Ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']);  // menyimpan data user baru Ajax
     //-------------------------------------------------------------------------------------------------------------
     Route::get('/{id}', [UserController::class, 'show']);        // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
     Route::put("/{id}", [UserController::class, 'update']);       // menyimpan perubahan data user
-    //---------------------------------------------Jobhseet 5 Praktikum 2------------------------------------------
+    //---------------------------------------------Jobhseet 6 Praktikum 2------------------------------------------
     Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); // menampilkan halaman form edit user Ajax
     Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); // menyimpan perubahan data user Ajax
-    //---------------------------------------------Jobsheet 5 Praktikum 3------------------------------------------
+    //---------------------------------------------Jobsheet 6 Praktikum 3------------------------------------------
     Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
     Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); // Untuk hapus data user Ajax
     //-------------------------------------------------------------------------------------------------------------
@@ -66,14 +66,14 @@ Route::group(['prefix' => 'level'], function () {
     Route::post('/list', [LevelController::class, 'list']);        // menampilkan data level dalam bentuk json untuk datatables
     Route::get('/create', [LevelController::class, 'create']);    // menampilkan halaman form tambah level
     Route::post('/', [LevelController::class, 'store']);          // menyimpan data level baru
-    //----------------------------------------------Jobsheet 5 Tugas 1---------------------------------------------
+    //----------------------------------------------Jobsheet 6 Tugas 1---------------------------------------------
     Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
     Route::post('/ajax', [LevelController::class, 'store_ajax']);
     //-------------------------------------------------------------------------------------------------------------
     Route::get('/{id}', [LevelController::class, 'show']);        // menampilkan detail level
     Route::get('/{id}/edit', [LevelController::class, 'edit']);  // menampilkan halaman form edit level
     Route::put("/{id}", [LevelController::class, 'update']);       // menyimpan perubahan data level
-    //----------------------------------------------Jobsheet 5 Tugas 1---------------------------------------------
+    //----------------------------------------------Jobsheet 6 Tugas 1---------------------------------------------
     Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);
     Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
     Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
@@ -87,9 +87,19 @@ Route::group(['prefix' => 'kategori'], function () {
     Route::post('/list', [KategoriController::class, 'list']);
     Route::get('/create', [KategoriController::class, 'create']);
     Route::post('/', [KategoriController::class, 'store']);
+    //-------------------------------------------------Jobsheet 6 Tugas 2-------------------------------------------
+    Route::get('/create_ajax', [KategoriController::class, 'create_ajax']);
+    Route::post('/ajax', [KategoriController::class, 'store_ajax']);
+    //-------------------------------------------------------------------------------------------------------------
     Route::get('/{id}', [KategoriController::class, 'show']);
     Route::get('/{id}/edit', [KategoriController::class, 'edit']);
     Route::put("/{id}", [KategoriController::class, 'update']);
+    //-------------------------------------------------Jobsheet 6 Tugas 2-------------------------------------------
+    Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
+    Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']);
+    Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);
+    Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']);
+    //-------------------------------------------------------------------------------------------------------------
     Route::delete('/{id}', [KategoriController::class, 'destroy']);
 });
 
