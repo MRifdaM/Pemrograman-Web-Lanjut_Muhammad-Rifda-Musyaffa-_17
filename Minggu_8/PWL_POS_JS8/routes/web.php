5b07dfc71b0c6837d3a81002fd12c7372d6a19a8
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-04-01 01:04:52 +0700

    Penambahan Jobsheet 8 Praktikum 1 : Implementasi Upload File untuk import data

--- Minggu_8/PWL_POS_JS8/routes/web.php
+++ Minggu_8/PWL_POS_JS8/routes/web.php
@@ -197,6 +197,8 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
             Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
             Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
             Route::delete('/{id}', [BarangController::class, 'destroy']);
+            Route::get('/import', [BarangController::class, 'import']);
+            Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
         });
     });
 
