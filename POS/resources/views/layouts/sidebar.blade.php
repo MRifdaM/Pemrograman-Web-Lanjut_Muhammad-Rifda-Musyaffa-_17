c22367d1c844e8acb38ff79411fc95ebd5cc93c9
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-04-09 16:33:08 +0700

    IMPLEMENTASI JOBSHEET 5 PRAKTIKUM 2:
    - Implementasi Layouting untuk halaman Dashboard

--- POS/resources/views/layouts/sidebar.blade.php
+++ POS/resources/views/layouts/sidebar.blade.php
@@ -66,6 +66,7 @@
                     <p>Transaksi Penjualan</p>
                 </a>
             </li>
+            <li class="nav-header">Data Supplier</li>
             <li class="nav-item">
                 <a href="{{ url('/supplier') }}" class="nav-link {{ ($activeMenu ==
                 'supplier') ? 'active' : '' }} ">
