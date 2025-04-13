c8a6336e9cf061d638c8ce3a3fcf1bbf2558a3df
Muhammad Rifda Musyaffa <140369893+MRifdaM@users.noreply.github.com>
2025-04-13 16:52:10 +0700

    IMPLEMENTASI JOBSHEET 7 :
    - Menambahkan fitur registrasi seperti menambahkan route registrasi, logika registrasi pengguna baru, dan halaman registrasi.

--- POS/app/Http/Controllers/AuthController.php
+++ POS/app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
+use App\Models\LevelModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -42,4 +45,43 @@ class AuthController extends Controller
         return redirect('login');
     }
 
+    public function register()
+    {
+        $levels = LevelModel::select('level_id', 'level_nama')
+                    ->where('level_kode', '!=', 'ADM')
+                    ->get();
+        return view('auth.register', compact('levels'));
+    }
+
+    public function postRegister(Request $request){
+        $rules = [
+            'level_id' => 'required|exists:m_level,level_id',
+            'username' => 'required|string|min:3|unique:m_user,username',
+            'nama' => 'required|string|max:100',
+            'password' => 'required|min:6|confirmed',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validasi Gagal',
+                'msgField' => $validator->errors()
+            ]);
+        }
+
+        UserModel::create([
+            'level_id' => $request->level_id,
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => $request->password,
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Registrasi Berhasil',
+            'redirect' => route('login')
+        ]);
+    }
 }
--- POS/routes/web.php
+++ POS/routes/web.php
@@ -253,6 +253,9 @@ Route::pattern('id', '[0-9]+'); // artinya ketika ada parameter {id}, maka harus
 Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('register', [AuthController::class, 'postRegister']);
+
 
 Route::middleware(['auth'])->group(function(){
     Route::get('/', [WelcomeController::class, 'index']);
@@ -376,7 +379,7 @@ Route::middleware(['auth'])->group(function(){
             Route::delete('/{id}', [StokController::class, 'destroy']);
         });
     });
-    
+
     Route::middleware(['authorize:ADM,MNG,STF,CSH'])->group(function(){
         Route::group(['prefix' => 'penjualan'], function () {
             Route::get('/', [PenjualanController::class, 'index']);
